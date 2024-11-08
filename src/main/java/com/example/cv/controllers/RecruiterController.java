8b63b3de07fa82a3ce066f4a468d3df159f60a84
abouelaecha01 <abouelaecha.19h@gmail.com>
2024-11-08 05:14:05 +0100

    owais

--- src/main/java/com/example/cv/controllers/RecruiterController.java
+++ src/main/java/com/example/cv/controllers/RecruiterController.java
@@ -23,7 +23,7 @@ public class RecruiterController {
 
     @PreAuthorize("hasRole('ROLE_RECRUITEUR') and hasAuthority('CREATE_OFFER')")
     @PostMapping("/createOffer")
-    public ResponseEntity<Offre> meryameController(@RequestBody OffreDTO offreDTO, @AuthenticationPrincipal User recruteur) {
+    public ResponseEntity<Offre> owaisController(@RequestBody OffreDTO offreDTO, @AuthenticationPrincipal User recruteur) {
         // N-call l-service w nsayb l'offre
         Offre createdOffer = recruiterService.createOffer(offreDTO, recruteur);
         return new ResponseEntity<>(createdOffer, HttpStatus.CREATED);
