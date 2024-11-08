3df4e47a684dbc7730fe576fb8c90bf2fd64ffa8
abouelaecha <hamzaabouelaecha@gmail.com>
2024-11-08 16:38:07 +0100

    2024dd

--- src/main/java/com/example/cv/zadclassjdida.java
+++ src/main/java/com/example/cv/zadclassjdida.java
@@ -3,7 +3,7 @@ package com.example.cv;
 public class zadclassjdida {
 
 
-    //khasna nzido chi haja f code owais
-    //khasna nzido chi haja f code ikram
+    //khasna nzido chi haja f code ilyas
+    //khasna nzido chi haja f code soukaina
 
 }
